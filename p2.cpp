ad764e15fd7fdccc2ae13ec4164a14704b0146ed
Matthew Tullo <mtullo1@binghamton.edu>
2019-11-24 18:16:50 -0500

    hello

--- p2.cpp
+++ p2.cpp
@@ -3,29 +3,76 @@
 using namespace std;
 
 //each cache line has size 32 bytes 
-int direct(vector<long long> addr, vector<string> func, int size){
-	long long cache[size];
-	int valid[size];
+int direct(vector<long long> addr, int size){
+  int blocks = size/32;
+  long long cache[blocks];
+	int valid[blocks];
 	int hit = 0;
 	for(int i = 0; i<size; i++){
 		cache[i] = -1;
 		valid[i] = 0;
 	}
 	for(int i = 0; i<addr.size(); i++){
-		int block_addr = addr[i]/size;
-		int index = addr[i]%size;
+	  long long addrB = addr[i]>>5;
+	  int index = addrB%(blocks);
+	  int tag = addrB/blocks;
 		if(valid[index] == 1){
-			if(cache[index] == block_addr)
+			if(cache[index] == tag)
 				hit++;
 			else
-				cache[index] = block_addr;
+				cache[index] = tag;
 		}	
 		if(valid[index] == 0){
 			valid[index] = 1;
-			cache[index] = block_addr;
+			cache[index] = tag;
 		}
 	}
 	return hit;
+}
+
+int set_associative(vector<long long>addr, int way){
+  int blocks = (16*1024)/32;
+  long long cache[blocks][way];
+  int hit = 0;
+  long long LRU[blocks][way];
+  for(int i = 0; i < blocks; i++){
+    for(int j = 0; j < way; j++){
+      cache[i][j] = -1;
+      LRU[i][j]=-1;
+    }
+  }
+  for(int x = 0; x<addr.size(); x++){
+    int tag = addr[x]/blocks;
+    for(int i = 0; i < blocks; i++){
+      for(int j = 0; j<way; j++){
+	if(cache[i][j] == tag){
+	  LRU[i][j] = x;
+	  hit++;
+	}
+	else if(cache[i][j] == -1){
+	  cache[i][j] = tag;
+	  LRU[i][j] = x;
+	}
+	else{
+	  int min = LRU[0][0];
+	  int minI;
+	  int minJ;
+	  for(int y = 0; y<blocks;y++){
+	    for(int q = 0; q<way; q++){
+	      if(LRU[y][q]<min){
+		min = LRU[y][q];
+		minI = y;
+		minJ = q;
+	      }
+	    }
+	  }
+	  cache[minI][minJ] = cache[i][j];
+	  LRU[minI][minJ] = x;
+	}
+      }
+    }
+  }
+  return hit;
 }
 
 int main(int argc, char *argv[]) {
@@ -53,9 +100,13 @@ int main(int argc, char *argv[]) {
 	ofstream cout(argv[2]);
 	vector<int> sizes{1024, 4096, 16384, 32768};
 	for(int i = 0; i<4; i++){
-		cout << direct(address, strld, sizes[i]) << ","<< address.size() << "; ";
+		cout << direct(address, sizes[i]) << ","<< address.size() << "; ";
 	}
-	cout << endl;
+        vector<int>sets{2,4,8,16};
+	  cout << endl;
+	  for(int i = 0; i<4; i++)
+	    cout << set_associative(address, sizes[i]) << "," << address.size() << "; ";
+	  cout << endl;
   return 0;
 }
 
