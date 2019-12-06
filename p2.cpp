046b2ed53d9e6a9f12fd9b65ab821eb712c8df1a
Matthew Tullo <mtullo1@binghamton.edu>
2019-12-05 20:47:52 -0500

    Update p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -161,6 +161,75 @@ int write_miss(vector<long long>addr, vector<string> behave, int way){
   }
   return hit;
 }
+int prefetch(vector<long long>addr, int way){
+  int sets = 512/way;
+  long long cache[sets][way];
+  int hit = 0;
+  int LRU[sets][way];
+  int full[sets];
+  int volume = 0;
+  for(int i = 0; i<sets; i++){
+    for(int j = 0; j<way; j++){
+      LRU[i][j] = -1;
+      cache[i][j] = -1;
+    }
+  }
+  for(int i = 0; i<addr.size()-1; i++){
+    long long temp = addr[i]>>5;
+    int tag = temp/sets;
+    int set = temp%sets;
+    long long temp2 = addr[i+1];
+    int tagP = temp2/sets;
+    int setP = temp2%sets;
+    bool cached = false;
+    bool cachedP = false;
+    for(int j = 0; j<way; j++){
+      if(cache[set][j] == -1){
+	cache[set][j] = tag;
+	LRU[sets][j] = i;
+	cached = true;
+      }
+      else if(cache[set][j] == tag){
+	hit++;
+	LRU[set][j] = i;
+	cached = true;
+      }
+      if(cache[setP][j] == -1){
+	cache[setP][j] = tagP;
+	LRU[setP][j] = i;
+	cachedP = true;
+      }
+      else if(cache[setP][j] == tagP){
+	hit++;
+	LRU[setP][j] = i;
+	cachedP = true;
+      }
+    }
+    if(!cached){
+      int lru = LRU[set][0];
+      int lruIndex = 0;
+      for(int j = 0; j<way; j++){
+	if(LRU[set][j] < lru){
+	  lruIndex = j;
+	}
+      }
+      LRU[set][lruIndex] = i;
+      cache[set][lruIndex] = tag;
+      }
+    if(!cachedP){
+      int lru = LRU[setP][0];
+      int lruIndex = 0;
+      for(int j = 0; j<way; j++){
+	if(LRU[setP][j] < lru){
+	  lruIndex = j;
+	}
+      }
+      LRU[setP][lruIndex] = i;
+      cache[setP][lruIndex] = tag;
+      }
+  }
+  return hit;
+}
 int main(int argc, char *argv[]) {
 
   // Temporary variables
